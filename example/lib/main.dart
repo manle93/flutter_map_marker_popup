2fa4f3cb535da097865c40817e5a91bfcd6756a6
Man Le <man.le@executionlab.asia>
2021-04-08 11:40:50 +0700

    fix dep

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
-import 'package:latlong/latlong.dart';
+import 'package:latlong2/latlong.dart';
 
 import 'example_popup.dart';
 
